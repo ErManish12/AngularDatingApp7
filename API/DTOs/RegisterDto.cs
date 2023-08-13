6eb975fc3a4d177a1dcec9dfd846ea7ae197939a
Manis Rawat <manish.rawatec@gmail.com>
2023-08-13 16:22:46 +0530

    End of section 7

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -7,7 +7,10 @@ namespace API.DTOs
         [Required]
         public string Username { get; set; }
 
-        [Required]  
+        [Required] 
+        [StringLength(8, MinimumLength = 4)] 
         public string Password { get; set; }
+
+        public int MyProperty { get; set; }
     }
 }
